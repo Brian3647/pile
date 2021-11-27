2503488536221e05d9abc2fcc7a158bf365f5d3a
Brian3647 <victoris3647@gmail.com>
2021-11-27 09:26:27 +0100

    style: Formatting

--- src/info.rs
+++ src/info.rs
@@ -1,42 +1,24 @@
 use std::path::PathBuf;
 
 pub fn get_info(path: PathBuf) -> String {
-
     let file_ext = path.extension();
-    let file_ext = match file_ext {   
+    let file_ext = match file_ext {
         Some(ext) => ext.to_str().unwrap(),
         None => "none",
-    };    
+    };
 
     match &*file_ext {
-
         // Programming language
-        "rs" => {
-            "Rust source code file.".into()
-        }
-        "go" => {
-            "Go source code file.".into()
-        }
-        "sh" => {
-            "Bash shell script file.".into()
-        }
+        "rs" => "Rust source code file.".into(),
+        "go" => "Go source code file.".into(),
+        "sh" => "Bash shell script file.".into(),
 
         // Media file
-        "png" => {
-            "Portable Network Graphics image file.".into()
-        }
-        "jpeg" => {
-            "Lossy compressed digital image file.".into()
-        }
+        "png" => "Portable Network Graphics image file.".into(),
+        "jpeg" => "Lossy compressed digital image file.".into(),
 
-        "none" => {
-            "Bash shell script file.".into()
-        }
+        "none" => "Bash shell script file.".into(),
 
-        _ => {
-            "Unknown file.".into()
-        }
+        _ => "Unknown file.".into(),
     }
-
 }
-
